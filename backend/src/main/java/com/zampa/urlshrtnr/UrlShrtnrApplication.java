6637ef1ab4593416d06f6aa9c7ef989b2ea75032
Felipe Zampa <zampa@zampas-air.localdomain>
2024-01-02 22:15:04 +0100

    First commit - empty project

---
+++ backend/src/main/java/com/zampa/urlshrtnr/UrlShrtnrApplication.java
@@ -0,0 +1,13 @@
+package com.zampa.urlshrtnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShrtnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshrtnrApplication.class, args);
+	}
+
+}
